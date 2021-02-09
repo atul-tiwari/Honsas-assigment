1d87e235eea4ee176557f55fcf935950bc8f1472
= <atult2208@gmail.com>
2021-02-09 19:43:02 +0530

    Fetch Product Details complete

--- _Fetch_Product_Data.py
+++ _Fetch_Product_Data.py
@@ -4,12 +4,61 @@ from selenium import webdriver
 
 class Product_Data:
 
+    def fetchName(self):
+        self.name = self._data_Div.find("h1").text
+
+    def fetchRating(self):
+        rate = self._data_Div.find(
+            "div", class_="Flex-sc-1lsr9yp-0 egCJRE ReviewBox").find("span").text
+
+        self.rating = float(rate[1:-1])
+
+    def fetchPrice(self):
+        self.price = float(self._data_Div.find("div", class_="price").text[1:])
+
+    def fetchDiscount(self):
+        temp = self. _data_Div.find("div", class_="price__discount")
+        if temp != None:
+            self.discount = (temp.text[1:-1].split(' ')[0])
+        else:
+            self.discount = "0%"
+
+    def fetchDiscription(self):
+        paragraph = (self._data_Div.find(
+            class_="Description-sc-40bbdp-0 gwjNeT").find_all("span"))
+
+        dis = ""
+        for p in paragraph:
+            dis += p.text
+
+        self.discription = dis
+
+    def fetchIngredients(self):
+
+        try:
+            coll_data = self._soup.find(
+                class_="woodmart-list").find_all("strong")
+
+            _ingredient = []
+            for i in coll_data:
+                _text = i.text[:i.text.find(':')]
+                _ingredient.append(_text.strip().lower())
+        except:
+            self._retry += 1
+            if self._retry <= 3:
+                self.fetchIngredients()
+            else:
+                print("Error in Fetching Ingredients")
+                return
+
+        self.ingredients = _ingredient
+
     def __init__(self, product_link=""):
         super().__init__()
         baseURL = "https://mamaearth.in"
 
         self.link = baseURL+product_link
-
+        self._retry = 0
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
 
@@ -20,23 +69,14 @@ class Product_Data:
         browser.execute_script("return document.readyState")
 
         html = browser.page_source
-        soup = BeautifulSoup(html, 'lxml')
+        self._soup = BeautifulSoup(html, 'lxml')
 
-        data_Div = soup.find(
+        self._data_Div = self._soup.find(
             class_="ProductDetails__Wrapper-sc-1htzzsm-0 fXidSC")
 
-        print(data_Div.find("h1").text)
-        print(data_Div.find(
-            "div", class_="Flex-sc-1lsr9yp-0 egCJRE ReviewBox").find("span").text)
-        print(data_Div.find("div", class_="price").text)
-        temp = data_Div.find("div", class_="price__discount")
-        print(temp.text if temp != None else "0%")
-
-        print(data_Div.find(class_="Description-sc-40bbdp-0 gwjNeT").find_all("span"))
-
-        coll_data = soup.find(class_="woodmart-list").find_all("strong")
-        print(coll_data)
-
-
-p1 = Product_Data(
-    "/product/charcoal-face-wash-scrub-combo")
+        self.fetchRating()
+        self.fetchName()
+        self.fetchPrice()
+        self.fetchDiscount()
+        self.fetchDiscription()
+        self.fetchIngredients()
